afb862cf392570a163027f871c16b78d36d4a7c5
nlom0218 <nlom0218@naver.com>
2021-05-27 07:49:38 +0900

    Create Account

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -1,7 +1,23 @@
 import client from "../client";
+import bcrypt from "bcrypt"
 
 export default {
   Query: {
     seeProfile: (_, { username }) => client.user.findUnique({ where: { username } })
+  },
+  Mutation: {
+    createAccount: async (_, { firstName, lastName, username, email, password, }) => {
+      const existingUser = await client.user.findFirst({ where: { OR: [{ username }, { email }] } })
+      const uglyPassword = await bcrypt.hash(password, 10)
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword
+        }
+      })
+    }
   }
 }
