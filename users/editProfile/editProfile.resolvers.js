9d23227be92fb5f698f7386ff77e8b254029a541
nlom0218 <nlom0218@naver.com>
2021-05-27 21:31:54 +0900

    protecting resolvers

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,34 +1,36 @@
 import client from "../../client"
 import bcrypt from "bcrypt"
+import { protectedResolover } from "../users.utils"
 
 export default {
   Mutation: {
-    editProfile: async (_, { firstName, lastName, username, email, password: newPassword }, { loggedInUser }) => {
-      console.log(loggedInUser);
-      let uglyPassword = null
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10)
+    editProfile: protectedResolover(
+      async (_, { firstName, lastName, username, email, password: newPassword }, { loggedInUser }) => {
+        let uglyPassword = null
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10)
+        }
+        const updatedUser = await client.user.update({
+          where: { id: loggedInUser.id },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword })
+          }
+        })
+        if (updatedUser.id) {
+          return {
+            ok: true
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Colud not update profile."
+          }
+        }
       }
-      const updatedUser = await client.user.update({
-        where: { id: loggedInUser.id },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword })
-        }
-      })
-      if (updatedUser.id) {
-        return {
-          ok: true
-        }
-      } else {
-        return {
-          ok: false,
-          error: "Colud not update profile."
-        }
-      }
-    }
+    )
   }
 }
--- users/users.utils.js
+++ users/users.utils.js
@@ -18,3 +18,13 @@ export const getUser = async (token) => {
     return null
   }
 }
+
+export const protectedResolover = (ourResolver) => (root, args, context, info) => {
+  if (!context.loggedInUser) {
+    return {
+      ok: false,
+      error: "Please log in to perform this action."
+    }
+  }
+  return ourResolver(root, args, context, info)
+}
