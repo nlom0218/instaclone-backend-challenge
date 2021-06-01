94d7e3a44cd786d27d8a317a8db105ed179f6503
nlom0218 <nlom0218@naver.com>
2021-06-01 20:35:17 +0900

    seeHashtag

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -4,5 +4,9 @@ export default {
   Photo: {
     user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
     hashtags: ({ id }) => client.hashtag.findMany({ where: { photos: { some: { id } } } })
+  },
+  Hashtag: {
+    totalPhotos: ({ id }) => client.photo.count({ where: { hashtags: { some: { id } } } }),
+    photos: ({ id }, { page }) => client.photo.findMany({ where: { hashtags: { some: { id } } } })
   }
 }
